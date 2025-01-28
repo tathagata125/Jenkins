9d3ebffe60e21ef1890549af65e7e87fa7da57f5
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2025-01-28 11:10:28 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python3
-# This Phython program will print Hellow World.
+# This Phython program will print Hellow World.......
 print("Hello World Demo......\n")
 print("Hello World Demo......\n")
 
